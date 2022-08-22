f4e793bf38db8a79d04b9151607e21ebaad4612e
Anthony Enriquez <32233059+AnthonyEnr1quez@users.noreply.github.com>
2022-08-22 02:29:57 -0500

    Update kinda_auto_manual.sh

--- kinda_auto_manual.sh
+++ kinda_auto_manual.sh
@@ -3,10 +3,8 @@
 sudo zypper dup
 sudo zypper in git make
 
-mkdir -p ~/projects/ansible
+git clone https://github.com/AnthonyEnr1quez/temp_wsl_tumbleweed_ansible.git ~/projects/ansible/temp_wsl_tumbleweed_ansible
 
-git clone -C ~/projects/ansible https://github.com/AnthonyEnr1quez/temp_wsl_tumbleweed_ansible.git
-
-cd ~/projects/ansible
+cd ~/projects/ansible/temp_wsl_tumbleweed_ansible
 make bootstrap_wsl
 ansible-playbook -K main.yaml -v
